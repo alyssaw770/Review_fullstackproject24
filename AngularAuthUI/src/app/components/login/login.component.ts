8991948ef256ef1e9a5e9e5ced79a2f4cf78f734
Alyssa Williams <109011613+alyssaw770@users.noreply.github.com>
2024-04-20 19:53:29 -0500

    form Validation

--- AngularAuthUI/src/app/components/login/login.component.ts
+++ AngularAuthUI/src/app/components/login/login.component.ts
@@ -1,20 +1,40 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import ValidateForm from '../../helpers/validateForm';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule, MatInputModule, MatButtonModule, MatIconModule, FormsModule, ReactiveFormsModule],
+  imports: [MatCardModule, MatInputModule, MatButtonModule, MatIconModule, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  username = new FormControl('', [Validators.required]);
-  password = new FormControl('',[Validators.required]);
+  loginForm!: FormGroup;
+
   hide = true;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert("Invalid form");
+    }
+  }
 }
--- AngularAuthUI/src/app/components/signup/signup.component.ts
+++ AngularAuthUI/src/app/components/signup/signup.component.ts
@@ -3,20 +3,41 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
+import ValidateForm from '../../helpers/validateForm';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatCardModule, FormsModule, ReactiveFormsModule, MatInput, MatLabel, MatFormFieldModule, MatButtonModule],
+  imports: [MatCardModule, FormsModule, ReactiveFormsModule, MatInput, MatLabel, MatFormFieldModule, MatButtonModule, MatIcon],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
-firstName = new FormControl('',[Validators.required]);
-lastName = new FormControl('',[Validators.required]);
-email = new FormControl('',[Validators.required]);
-telephone = new FormControl('');
-dateOfBirth = new FormControl('',[Validators.required]);
+  signupForm!: FormGroup;
+  hide = true;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['',Validators.required],
+      password: ['', Validators.required],
+      telephone: [''],
+      dateOfBirth: ['', Validators.required]
+    })
+
+  }
+
+  onSignup() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+    } else {
+      ValidateForm.validateAllFormFields(this.signupForm);
+    }
+  }
 }
